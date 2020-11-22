32965619297a10c69a92981e8096eb85ca725539
Brian Boucher <bmboucher@gmail.com>
2020-11-22 12:06:54 -0500

    Adding top design with IP Integrator

---
+++ vivado/synth_hw/synth_hw.srcs/sources_1/bd/top/hdl/top_wrapper.v
@@ -0,0 +1,24 @@
+//Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+//Date        : Sun Nov 22 12:05:41 2020
+//Host        : DESKTOP-VU7H966 running 64-bit major release  (build 9200)
+//Command     : generate_target top_wrapper.bd
+//Design      : top_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module top_wrapper
+   (reset,
+    sys_clock);
+  input reset;
+  input sys_clock;
+
+  wire reset;
+  wire sys_clock;
+
+  top top_i
+       (.reset(reset),
+        .sys_clock(sys_clock));
+endmodule
